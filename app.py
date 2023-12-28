978c259644ce4904a77e883e7ba8004544931466
Faidullah Moftah <faidullamoftah2@gmail.com>
2023-12-28 02:51:20 +0200

    comit

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from urllib import request
 
 from flask import Flask, request, jsonify
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)  # This enables CORS for all routes in your Flask app
 
 
 @app.route('/')
@@ -12,7 +15,7 @@ databases = {}
 
 @app.route('/add_item', methods = ['POST'])
 def add_item():
-    location = request.args.get('location')
+    """location = request.args.get('location')
     name = request.args.get('name')
     qty = request.args.get('quantity')
 
@@ -21,7 +24,11 @@ def add_item():
         return jsonify({'message': 'Item added successfully'}, 200)
     except:
         return jsonify({'error': 'Something wend wrong, please try again'}), 400
-
+    """
+    print(request.form)
+    received_data = request.data.decode('utf-8')
+    print("Request JSON Data:", received_data)
+    return jsonify({'were here' : 1})
 @app.route('/update_item', methods=['POST'])
 def update_item():
     # Extracting values from the URL query string (GET parameters)
